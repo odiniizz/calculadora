13e3f68305786f536cd76359377721f79807bdcb
odiniizz <jcesarddiniz05@gmail.com>
2025-01-07 13:29:20 -0300

    c

--- resources/views/resposta.blade.php
+++ resources/views/resposta.blade.php
@@ -11,6 +11,16 @@
     <div class="col-md-3">Valor atualizado</div>
 </div>
 
+@foreach ($dados as $item)
+
+    <div>
+        <div class="col-md-3"> {{$item["mes"]}} </div>
+        <div class="col-md-6"> R$ {{$item["capitalInicial"]}} </div>
+        <div class="col-md-3"> {{$item["capitalAtualizado"]}} </div>
+    </div>
+
+@endforeach
+
 <button onclick="window.location.href='/';" type="button" 
     class="btn btn-lg btn-primary btn-block botao">Voltar</button>
 
